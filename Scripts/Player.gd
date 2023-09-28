87091e48bc2c527e51524213c13b6cdb54de61ab
MaoticaGames <MaoticaGames@gmail.com>
2023-09-27 20:28:48 -0400

    Initialized.

---
+++ Scripts/Player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const gravity = 1200
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	velocity.x = SPEED
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction = Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	update_animation()
+
+func update_animation():
+	if velocity.y > 0:
+		animated_sprite.play("Jump")
+	elif velocity.x != 0:
+		animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Stop")
+
