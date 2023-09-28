c29bf098592da7f6baaa10fa7ec79e6e12bfa21e
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-09-27 21:58:43 -0400

    Adjusted Animations, Buttons and Scenes
    Adjusted animation for left/right running and jumping.
    Added archives/settings buttons and linked to corresponding scenes.
    Added back buttons for archives/settings.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -22,20 +22,25 @@ func _physics_process(delta):
 	velocity.x = SPEED
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	update_animation()
 
 func update_animation():
 	if velocity.y > 0:
-		animated_sprite.play("Jump")
-	elif velocity.x != 0:
-		animated_sprite.play("Run")
+		if velocity.x > 0:
+			animated_sprite.play("JumpR")
+		elif velocity.x < 0:
+			animated_sprite.play("JumpL")
+	elif velocity.x < 0:
+		animated_sprite.play("RunL")
+	elif velocity.x > 0:
+		animated_sprite.play("RunR")
 	else:
 		animated_sprite.play("Stop")
 
