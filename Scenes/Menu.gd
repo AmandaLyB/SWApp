c8e3aaeaadb9e9026868e337278d4ddbcb895dcc
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-09-29 16:34:41 -0400

    Adjusted Menu, Settings, Archives.
    Updated menu, settings and archives with an updated look.

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-func _on_texture_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Map.tscn")
-
-func _on_quit_pressed():
-	get_tree().quit()
-	
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Settings.tscn")
 	
 func _on_archives_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Archives.tscn")
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Map.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
