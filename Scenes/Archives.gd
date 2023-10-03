eec18b8de45a2285e65a91f3e1978a0a8713e70a
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-10-03 18:06:00 -0400

    Partially completed the SWAPI on archives page.
    I cant seem to figure out how to split the data it retrieves so I can connect it to various labels or a single lable. (I do have it allowed for all 82 characters on SWAPI available to pull from.

--- Scenes/Archives.gd
+++ Scenes/Archives.gd
@@ -1,7 +1,30 @@
 extends Node2D
 
+var currentCharacterIndex: int = 0
+var maxCharacterIndex: int = 82
+
 func _ready():
-	var sound_manager = SoundManager
+	$HTTPRequest.connect("character_data_fetched", doSomething)
+	parseCharacterData(currentCharacterIndex)
+	
+func parseCharacterData(characterIndex):
+	var apiUrl = "https://swapi.dev/api/people/" + str(characterIndex +1) + "/"
+	$HTTPRequest.request(apiUrl)
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	currentCharacterIndex -= 1
+	if currentCharacterIndex < 0:
+		currentCharacterIndex = maxCharacterIndex
+	parseCharacterData(currentCharacterIndex)
+
+func _on_next_pressed():
+	currentCharacterIndex += 1
+	if currentCharacterIndex > maxCharacterIndex:
+		currentCharacterIndex = 0
+	parseCharacterData(currentCharacterIndex)
+
+func doSomething(data: String):
+	$MarginContainer/VBoxContainer/swapiname.text = data
