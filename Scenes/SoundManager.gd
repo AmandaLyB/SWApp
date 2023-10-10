c7dd4395216766560bf5aa0e13ebd41bba5fdbb9
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-10-10 17:44:33 -0400

    Adjusted the menu page so when you select a play state.
    I added a skip to the cinematic [mainly for debugging purposes].
    Changed the menu so that its seamless when you click play and select a level.

--- Scenes/SoundManager.gd
+++ Scenes/SoundManager.gd
@@ -21,9 +21,9 @@ func _ready():
 		var random_music_path = music_tracks[random_track_index]
 		play_music(random_music_path)
 	
-	set_master_volume(0.2)
+	set_master_volume(0.5)
 	set_music_volume(0.2)
-	set_effects_volume(0.7)
+	#set_effects_volume(0.7)
 
 func play_music(music_path: String):
 	background_music.stream = load(music_path)
@@ -41,22 +41,22 @@ func get_music_volume() -> float:
 func set_music_volume(volume: float) -> void:
 	background_music.volume_db = linear_to_db(volume)
 
-func get_effects_volume() -> float:
-	return AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Effects"))
+#func get_effects_volume() -> float:
+	#return AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Effects"))
 
-func set_effects_volume(volume: float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Effects"), linear_to_db(volume))
+#func set_effects_volume(volume: float) -> void:
+	#AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Effects"), linear_to_db(volume))
 
 func save_settings() -> void:
 	var config_file = ConfigFile.new()
 	config_file.set_section_key("Audio", "master_volume", str(get_master_volume()))
 	config_file.set_section_key("Audio", "music_volume", str(get_music_volume()))
-	config_file.set_section_key("Audio", "effects_volume", str(get_effects_volume()))
+	#config_file.set_section_key("Audio", "effects_volume", str(get_effects_volume()))
 	config_file.save("user://audio_settings.cfg")
 
 func load_settings() -> void:
 	var config_file = ConfigFile.new()
 	if config_file.load("user://audio_settings.cfg") == OK:
-		set_master_volume(float(config_file.get_section_key("Audio", "master_volume", "0.2")))
+		set_master_volume(float(config_file.get_section_key("Audio", "master_volume", "0.5")))
 		set_music_volume(float(config_file.get_section_key("Audio", "music_volume", "0.2")))
-		set_effects_volume(float(config_file.get_section_key("Audio", "effects_volume", "0.8")))
+		#set_effects_volume(float(config_file.get_section_key("Audio", "effects_volume", "0.8")))
