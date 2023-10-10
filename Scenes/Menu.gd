cf5f80e4405580d9a8d72ef96054861c45dc2c5e
jcolon139 <112901644+jcolon139@users.noreply.github.com>
2023-10-09 22:17:25 -0400

    Changed menu Play button to Level Select
    Changed target of Play button from Map to Level Select.

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -10,7 +10,7 @@ func _on_archives_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Archives.tscn")
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Map.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_Select.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
