bf069044cd2a8c748c0292900984da9acfba2207
jcolon139 <112901644+jcolon139@users.noreply.github.com>
2023-10-09 22:06:36 -0400

    New branch created with previous changes.
    Intro video added and level select screen.

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -11,3 +11,12 @@ func _on_settings_pressed():
 	
 func _on_archives_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Archives.tscn")
+<<<<<<< Updated upstream
+=======
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level_Select.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+>>>>>>> Stashed changes
