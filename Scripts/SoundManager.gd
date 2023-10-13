e3b2c37af03397c437379be9245e4411de774dfb
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-10-12 22:22:09 -0400

    Added Touch Controls. Updated Buttons. Fixed Music. (#25)
    I added touch controls for left, right, and jump. I updated the skip cinematic button (gave it a theme). I updated the map UI to have settings instead of "menu" and when you click it, it pauses the game. You are able to restart level and quit to menu from inside the settings page in game.

--- Scripts/SoundManager.gd
+++ Scripts/SoundManager.gd
@@ -1,32 +1,14 @@
 extends Node2D
 
-var sound_effects: Dictionary = {}
+#var sound_effects: Dictionary = {}
 var background_music: AudioStreamPlayer = AudioStreamPlayer.new()
 
-var music_tracks: Array = [
-	"res://Audio/Background/starfleet-command-150605.mp3",
-	"res://Audio/Background/cinematic-battle-music-star-wars-style-148641.mp3",
-	"res://Audio/Background/star-wars-style-march-165111.mp3",
-	"res://Audio/Background/war-is-coming-103662.mp3"
-]
-
 func _ready():
-	if background_music.get_parent() == null:
-		background_music.name = "BackgroundMusic"
-		add_child(background_music)
-		background_music.bus="music"
-	
-	if !background_music.is_playing():
-		var random_track_index = randi() % music_tracks.size()
-		var random_music_path = music_tracks[random_track_index]
-		play_music(random_music_path)
-	
-	set_master_volume(0.5)
+	set_master_volume(0.2)
 	set_music_volume(0.2)
 	#set_effects_volume(0.7)
 
-func play_music(music_path: String):
-	background_music.stream = load(music_path)
+func play_music():
 	background_music.play()
 
 func get_master_volume() -> float:
@@ -58,5 +40,5 @@ func load_settings() -> void:
 	var config_file = ConfigFile.new()
 	if config_file.load("user://audio_settings.cfg") == OK:
 		set_master_volume(float(config_file.get_section_key("Audio", "master_volume", "0.5")))
-		set_music_volume(float(config_file.get_section_key("Audio", "music_volume", "0.2")))
+		set_music_volume(float(config_file.get_section_key("Audio", "music_volume", "0.5")))
 		#set_effects_volume(float(config_file.get_section_key("Audio", "effects_volume", "0.8")))
