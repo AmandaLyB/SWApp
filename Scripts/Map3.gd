dd99b53ba919831a81e9c3275c1ac78056490897
Andrew Goodman <59290966+drewgoodyman@users.noreply.github.com>
2023-11-10 13:26:10 -0500

    Updated map3
    Definitely harder and don't fall into the acid.

--- Scripts/Map3.gd
+++ Scripts/Map3.gd
@@ -21,3 +21,8 @@ func _on_quit_pressed() -> void:
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 	Engine.time_scale = 1
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Player:
+		area.get_tree().reload_current_scene()
